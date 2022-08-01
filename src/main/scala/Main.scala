06ade6d4672f4400e2aa90ad7b9e1b6306f51587
philipschwarz <philip.johann.schwarz@gmail.com>
2022-08-01 21:32:21 +0100

    more improvements

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -24,15 +24,15 @@ extension (m: Nat)
 
 ////////////////////////////////////////////////////////////////////////////
 
-sealed trait Semigroup[A]:
+trait Semigroup[A]:
   def combine(x: A, y: A): A
 
 object Semigroup:
 
   extension [A](lhs: A)(using m: Semigroup[A])
-    def |+|(rhs: A): A = m.combine(lhs,rhs)
+    def ⨁(rhs: A): A = m.combine(lhs,rhs)
 
-sealed trait Monoid[A] extends Semigroup[A]:
+trait Monoid[A] extends Semigroup[A]:
   def unit: A
 
 ////////////////////////////////////////////////////////////////////////////
@@ -45,7 +45,7 @@ object List:
 
   def apply[A](as: A*): List[A] = as match
     case Seq() => Nil
-    case _ => Cons(as.head,apply(as.tail*))
+    case _ => Cons(as.head,List(as.tail*))
 
   def nil[A]: List[A] = Nil
 
@@ -82,7 +82,7 @@ given Monoid[Nat] with
   def unit: Nat = Zero
   def combine(x: Nat, y: Nat): Nat = x + y
 
-val natMultMonoid : Monoid[Nat] = new Monoid[Nat]:
+val natMultMonoid = new Monoid[Nat]:
   def unit: Nat = Succ(Zero)
   def combine(x: Nat, y: Nat): Nat = x * y
 
@@ -90,12 +90,12 @@ given ListMonoid[A]: Monoid[List[A]] with
   def unit: List[A] = Nil
   def combine(lhs: List[A], rhs: List[A]): List[A] = lhs ++ rhs
 
-given OptionMonoid[M](using m: Monoid[M]): Monoid[Option[M]] with
+given OptionMonoid[M:Semigroup]: Monoid[Option[M]] with
   def unit: Option[M] = None
   def combine(ox: Option[M], oy: Option[M]): Option[M] = (ox,oy) match
     case (None,_) => oy
     case (_, None) => ox
-    case (Some(x),Some(y)) => Some(x |+| y)
+    case (Some(x),Some(y)) => Some(x ⨁ y)
 
 @main def main: Unit =
 
@@ -116,76 +116,89 @@ given OptionMonoid[M](using m: Monoid[M]): Monoid[Option[M]] with
 
   val ten = one + two + three + four
 
+  assert(two * one == two)
+  assert(one * two == two)
   assert(two * three == six)
 
-  assert( (two |+| three) == five)
+  assert( summon[Monoid[Nat]].combine(two,three) == five)
+  assert( (two ⨁ three) == five)
+  assert( (one ⨁ two ⨁ three) == six)
 
-  val numbers: List[Nat] =
-    Cons(one, Cons(two, Nil))
+  val oneTwo = Cons(one, Cons(two, Nil))
+  val threeFour = Cons(three, Cons(four, Nil))
+
+  assert(append(oneTwo,threeFour) == Cons(one, Cons(two, Cons(three, Cons(four, Nil)))))
+  assert(oneTwo ++ threeFour == Cons(one, Cons(two, Cons(three, Cons(four, Nil)))))
+
+  assert(List(one,two,three) == Cons(one, Cons(two, Cons(three, Nil))))
+
+  val numbers: List[Nat] = List(one,two)
 
   val moreNumbers: List[Nat] = List(three, four)
 
   val allNumbers: List[Nat] = Cons(one, Cons(two, Cons(three, Cons(four, Nil))))
 
-  assert(List(one,two,three) == Cons(one, Cons(two, Cons(three, Nil))))
-
   val yetMoreNumbers: List[Nat] = List(five,six)
 
   // use List[A]'s ++ function
-  assert(numbers ++ moreNumbers == allNumbers)
+  assert(List(one,two) ++ List(three, four) == List(one,two,three,four))
   // use List monoid's combine function
-  assert(summon[Monoid[List[Nat]]].combine(numbers,moreNumbers) == allNumbers)
+  assert(summon[Monoid[List[Nat]]].combine(List(one,two),List(three, four)) == List(one,two,three,four))
   // use List monoid's infix combine operator
-  assert((numbers |+| moreNumbers) == allNumbers)
+  assert((List(one,two) ⨁ List(three, four) ⨁ Nil) == List(one,two,three,four))
 
   // fold List[Nat] with (+,0)
-  assert(fold(allNumbers) == one + two + three + four)
+  assert(fold(List(one,two,three,four)) == one + two + three + four)
   assert(fold(nil[Nat]) == Zero)
   // fold List[Nat] with (*,1)
-  assert(fold(allNumbers)(using natMultMonoid) == one * two * three * four)
-  assert(fold(Nil:List[Nat])(using natMultMonoid) == one)
+  assert(fold(List(one,two,three,four))(using natMultMonoid) == one * two * three * four)
+  assert(fold(nil[Nat])(using natMultMonoid) == one)
   // fold List[List[Nat] with (List,combine)
-  assert(fold(List(numbers,moreNumbers,yetMoreNumbers)) == List(one,two,three,four,five,six))
+  assert(fold(List(List(one,two),Nil,List(three, four),List(five,six))) == List(one,two,three,four,five,six))
 
   // use Option monoid's combine function
+  assert(summon[Monoid[Option[Nat]]].combine(Some(two),Some(three)) == Some(five))
   assert(summon[Monoid[Option[Nat]]].combine(Some(two),None) == Some(two))
+  assert(summon[Monoid[Option[Nat]]].combine(none[Nat],Some(two)) == Some(two))
+  assert(summon[Monoid[Option[Nat]]].combine(none[Nat],None) == None)
   // use Option monoid's infix combine operator
-  assert((some(two) |+| None) == Some(two))
-  assert((none[Nat] |+| Some(two)) == Some(two))
-  assert((some(two) |+| Some(three)) == Some(five))
-  assert((none[Nat] |+| None) == None)
-  assert(Option.fold(none[Nat] |+| None) == zero)
-
-  val optionalNumbers: List[Option[Nat]] = List(Some(two),None,Some(three))
-
-  val moreOptionalNumbers: List[Option[Nat]] = List(Some(one),None,Some(four))
+  assert((some(two) ⨁ None) == Some(two))
+  assert((none[Nat] ⨁ Some(two)) == Some(two))
+  assert((some(two) ⨁ Some(three)) == Some(five))
+  assert((none[Nat] ⨁ None) == None)
+  assert(Option.fold(none[Nat] ⨁ None) == zero)
 
   val allOptionalNumbers: List[Option[Nat]] = List(Some(two),None,Some(three),Some(one),None,Some(four))
 
-  val yetMoreOptionalNumbers: List[Option[Nat]] = List(Some(five),None,Some(six))
-
   // fold List of Option[Nat] with (Option[Nat],combine)
-  assert(fold(optionalNumbers) == Some(five))
-  assert(fold(Cons(Some(two), Cons(None, Cons(Some(three), Nil)))) == Some(five))
   assert(fold(List(Some(two),None,Some(three))) == Some(five))
+  assert(fold(nil[Option[Nat]]) == None)
 
   // combine two lists of optional numbers
-  assert((optionalNumbers ++ moreOptionalNumbers) == allOptionalNumbers)
+  assert((List(Some(one),None,Some(three)) ++ List(Some(four),None,Some(six)))
+          == List(Some(one),None,Some(three),Some(four),None,Some(six)))
   // use List[Option[Nat]] monoid's combine function
-  assert(summon[Monoid[List[Option[Nat]]]].combine(optionalNumbers,moreOptionalNumbers) == allOptionalNumbers)
+  assert(summon[Monoid[List[Option[Nat]]]].combine(List(Some(one),None,Some(two)),List(Some(three),None,Some(four)))
+         == List(Some(one),None,Some(two),Some(three),None,Some(four)))
   // use List[Option[Nat]] monoid's infix combine operator
-  assert((optionalNumbers |+| moreOptionalNumbers) == allOptionalNumbers)
+  assert((List(Some(one),None,Some(two)) ⨁ List(Some(three),None,Some(four)))
+          == List(Some(one),None,Some(two),Some(three),None,Some(four)))
   // fold the combination of two List[Option[Nat]] using (Option[Nat],combine)
-  assert(fold(optionalNumbers |+| moreOptionalNumbers) == Some(ten))
+  assert(fold(List(Some(one),None,Some(two)) ⨁ List(Some(three),None,Some(four))) == Some(one + two + three + four))
 
-  val listsOfOptionalNumbers = List(optionalNumbers,moreOptionalNumbers,yetMoreOptionalNumbers)
   //val listsOfOptionalNumbers = Cons(optionalNumbers,Cons(moreOptionalNumbers,Cons(yetMoreOptionalNumbers,Nil)))
 
   val twentyOne = five + five + five + six
 
   // fold the list of List[Option[Nat]] into a List[Option[Nat]] and then fold the latter using (Option[Nat],combine)
-  assert(fold(fold(listsOfOptionalNumbers)) == Some(twentyOne))
+  assert(
+    fold(
+      fold(
+        List(List(Some(one), None, Some(two)),
+             List(Some(three), None, Some(four)),
+             List(Some(five), None, Some(six)))))
+    == Some(one + two + three + four + five + six))
 
   // combine three Option[List[Nat]] into a single Option[List[Nat]] by combining the lists using (List,combines)
-  assert((some(List(one,two)) |+| None |+| Some(List(three,four))) == Some(List(one,two,three,four)))
-  assert(Option.fold(some(List(one,two)) |+| None |+| Some(List(three,four))) == List(one,two,three,four))
+  assert((some(List(one,two)) ⨁ None ⨁ Some(List(three,four))) == Some(List(one,two,three,four)))
+  assert(Option.fold(some(List(one,two)) ⨁ None ⨁ Some(List(three,four))) == List(one,two,three,four))
